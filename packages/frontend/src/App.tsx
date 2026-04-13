334e1099c694d502f0b35045b0f52c81726bccef
Ikuma Yamashita <me@ikuma.cloud>
2026-04-13 21:26:14 +0900

    refactor: move ElmToggleTheme to a fixed position for improved accessibility

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { ElmToggleTheme } from "@elmethis/react";
 import { ChatContainer } from "./components/ChatContainer";
 
 function App() {
   return (
     <div>
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          padding: ".5rem",
+        }}
+      >
+        <ElmToggleTheme />
+      </div>
       <ChatContainer />
     </div>
   );
